20585c58d2213c49e9af767f37e51a66f1d48820
Nathan Totten <nathan.james.totten@gmail.com>
2021-07-08 15:50:52 +0100

    Added functions to generate random character.

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,6 @@
+sessionStorage.setItem('consonants', 'BCDFGHJKLMNPQRSTVWXYZ');
+sessionStorage.setItem('vowels', 'AEIOU');
+
 // Map the available themes that can be cycled through.
 
 const themeMap = {
@@ -35,3 +38,61 @@ function toggleTheme() {
 // Set the on click property of the theme changing button to call the toggle theme function.
 
 document.getElementById('themeButton').onclick = toggleTheme;
+
+// Generate a random consonant.
+
+function randomConsonant() {
+    var randCons = '';
+    let consonants = sessionStorage.getItem('consonants');
+    randCons += consonants.charAt(Math.floor(Math.random() * consonants.length))
+    updatedConsonants = consonants.replace(randCons, '');
+    sessionStorage.setItem('consonants', updatedConsonants);    // console.log(randCons)
+    return randCons;
+}
+
+// Generate a random vowel.
+
+function randomVowel() {
+    var randVowel = '';
+    let vowels = sessionStorage.getItem('vowels');
+    randVowel += vowels.charAt(Math.floor(Math.random() * vowels.length))
+    updatedVowels = vowels.replace(randVowel, '');
+    sessionStorage.setItem('vowels', updatedVowels);
+    return randVowel;
+}
+
+// Test the random consonant feature.
+
+// random1 = randomConsonant();
+// random2 = randomConsonant();
+// random3 = randomConsonant();
+// random4 = randomConsonant();
+// random5 = randomConsonant();
+// random6 = randomConsonant();
+// random7 = randomConsonant();
+// random8 = randomConsonant();
+
+// remainingConsonants = sessionStorage.getItem('consonants');
+
+// console.log(random1)
+// console.log(random2)
+// console.log(random3)
+// console.log(random4)
+// console.log(random5)
+// console.log(random6)
+// console.log(random7)
+// console.log(remainingConsonants)
+
+// Test the random vowel feature.
+
+// random1 = randomVowel();
+// random2 = randomVowel();
+// random3 = randomVowel();
+
+// remainingVowels = sessionStorage.getItem('vowels');
+
+// console.log(random1)
+// console.log(random2)
+// console.log(random3)
+
+// console.log('remaining vowels: ' + remainingVowels)
